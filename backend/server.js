e1b7a7f2c9da2f35e4713316b36cd47485dbe8b2
shubham sharma <devopsmail4u@gmail.com>
2026-01-22 01:40:52 +0530

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: '*' }
+});
+
+let waitingUser = null;
+const activeChats = new Map(); // socketId -> partnerId
+
+io.on('connection', socket => {
+  console.log('User connected:', socket.id);
+
+  socket.on('start_search', () => {
+    if (activeChats.has(socket.id)) return;
+
+    if (waitingUser && waitingUser !== socket.id) {
+      const partner = waitingUser;
+      waitingUser = null;
+
+      activeChats.set(socket.id, partner);
+      activeChats.set(partner, socket.id);
+
+      socket.emit('matched');
+      io.to(partner).emit('matched');
+    } else {
+      waitingUser = socket.id;
+      socket.emit('searching');
+    }
+  });
+
+  socket.on('send_message', msg => {
+    const partner = activeChats.get(socket.id);
+    if (partner) {
+      io.to(partner).emit('message', msg);
+    }
+  });
+
+  socket.on('end_chat', () => {
+    endSession(socket.id, 'chat_ended');
+  });
+
+  socket.on('disconnect', () => {
+    endSession(socket.id, 'partner_left');
+    if (waitingUser === socket.id) waitingUser = null;
+    console.log('User disconnected:', socket.id);
+  });
+
+  function endSession(id, event) {
+    const partner = activeChats.get(id);
+    if (partner) {
+      io.to(partner).emit(event);
+      activeChats.delete(partner);
+    }
+    activeChats.delete(id);
+  }
+});
+
+server.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
